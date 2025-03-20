a3bf180eeb44fac0e234e183dd098b5a3cae26dd
= <james.a.padden@gmail.com>
2025-03-20 16:51:04 -0700

    Created database variable

--- todo/__init__.py
+++ todo/__init__.py
@@ -7,3 +7,8 @@ from .views import views
 app.register_blueprint(views)
 from .auth import auth
 app.register_blueprint(auth)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SECRET_KEY"] = "ENTER YOUR SECRET KEY"
+db = SQLAlchemy()
+db.init_app(app)
