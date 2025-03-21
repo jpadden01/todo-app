f9624f99edf7ab40884a2c5fd34b0b029d8f3895
= <james.a.padden@gmail.com>
2025-03-20 17:12:10 -0700

    Created signup form

--- todo/auth.py
+++ todo/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
@@ -10,7 +10,7 @@ def login():
 @auth.route('/signup')
 def signup():
     # TODO: SIGN UP PAGE
-    return '<p>Sign Up</p>'
+    return render_template('signup.html')
 
 @auth.route('/logout')
 def logout():
