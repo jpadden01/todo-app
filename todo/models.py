a69336a39ccd82535e4afe9a5d817e0963523d2d
= <james.a.padden@gmail.com>
2025-03-20 17:03:53 -0700

    Added Task model and created relationship between User/Task models

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,9 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
+    tasks = db.relationship('Task', backref='author', lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.String(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
